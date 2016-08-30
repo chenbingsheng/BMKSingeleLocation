edc17a221e5bc8dbeb40fb993af2da1635483cbb
_chenbingsheng <chenbingsheng@hzjytech.com>
2016-08-30 14:09:43 +0800

    Initial Commit

---
+++ BMKSingeleLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BMKSingeleLocation
+//
+//  Created by cbs on 16/8/30.
+//  Copyright © 2016年 CBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BMKSingeleLocation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMKSingeleLocation
+//
+//  Created by cbs on 16/8/30.
+//  Copyright © 2016年 CBS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
